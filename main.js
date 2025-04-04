5347b151ee9d946741ac2c927865ab34cb393685
unknown <srijancodex@gmail.com>
2025-04-04 21:40:32 +0530

    creating scene hirearchy and adding texture

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 import * as THREE from 'three';
 
 const images = [
-    'deer.jng',
-    'bird1.jng',
-    'bird2.jng',
-    'bird3.jng',
-    'sqrl.jng'
+    'deer.jpg',
+    'bird1.jpg',
+    'sqrl.jpg',
+    'bird2.jpg',
+    'butterfly.jpg',
+    'bird3.jpg'
 ];
 
 const titles = [
     'Deer Captured',
     'Bird Moments One',
-    'Bird Moments Two',
-    'Bird Moments Three',
     'Squirell Captured',
+    'Bird Moments Two',
+    'Butterfly Capturd',
+    'Bird Moments Three',
 ]
 
 const artist = [
@@ -22,34 +24,50 @@ const artist = [
     'martin-bennie',
     'doncoombenz',
     'david-clode',
+    'david-clode',
 ]
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const textureLoader = new THREE.TextureLoader();
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-camera.position.z = 5;
+const rootNode = new THREE.Object3D();
+scene.add(rootNode)
 
-function animate() {
+let count = 6;
+for (let i = 0; i < count; i++) {
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    const texture = textureLoader.load(images[i]);
+    texture.colorSpace = THREE.SRGBColorSpace;
 
-    renderer.render(scene, camera);
+    const baseNode = new THREE.Object3D();
+    baseNode.rotation.y = i * (2 * Math.PI / count);
+    rootNode.add(baseNode);
 
+    const artwork = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 2, 0.1),
+        new THREE.MeshBasicMaterial({
+            map: texture
+        })
+    );
+    artwork.position.z = -4;
+    baseNode.add(artwork);
 }
 
-window.addEventListener('resize' , ()=>{
-    camera.aspect = window.innerWidth/ window.innerHeight;
+
+function animate() {
+    rootNode.rotation.y += 0.002;
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 })
